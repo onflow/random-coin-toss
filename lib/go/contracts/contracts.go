e65b0aa4e92a59308192003c1d8288620a830130
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2023-10-26 14:44:39 -0500

    add go script & txn templates

--- lib/go/contracts/contracts.go
+++ lib/go/contracts/contracts.go
@@ -3,66 +3,66 @@ package contracts
 //go:generate go run github.com/kevinburke/go-bindata/go-bindata -prefix ../../../contracts -o internal/assets/assets.go -pkg assets -nometadata -nomemcopy ../../../contracts
 
 import (
-	"regexp"
+    "regexp"
 
-	_ "github.com/kevinburke/go-bindata"
+    _ "github.com/kevinburke/go-bindata"
 
-	"github.com/onflow/flow-go-sdk"
+    "github.com/onflow/flow-go-sdk"
 
-	"github.com/onflow/random-coin-toss/lib/go/contracts/internal/assets"
+    "github.com/onflow/random-coin-toss/lib/go/contracts/internal/assets"   
 )
 
 var (
-	placeholderNonFungibleToken   		= regexp.MustCompile(`"NonFungibleToken"`)
-	placeholderFungibleToken			= regexp.MustCompile(`"FungibleToken"`)
-	placeholderResolver					= regexp.MustCompile(`"ViewResolver"`)
-	placeholderMetadataViews      		= regexp.MustCompile(`"MetadataViews"`)
-	placeholderRandomBeaconHistory		= regexp.MustCompile(`"RandomBeaconHistory"`)
-	placeholderPseudoRandomGenerator	= regexp.MustCompile(`"PseudoRandomGenerator"`)
-	placeholderCoinToss			      	= regexp.MustCompile(`"CoinToss"`)
+    placeholderNonFungibleToken   		= regexp.MustCompile(`"NonFungibleToken"`)
+    placeholderFungibleToken			= regexp.MustCompile(`"FungibleToken"`)
+    placeholderResolver					= regexp.MustCompile(`"ViewResolver"`)
+    placeholderMetadataViews      		= regexp.MustCompile(`"MetadataViews"`)
+    placeholderRandomBeaconHistory		= regexp.MustCompile(`"RandomBeaconHistory"`)
+    placeholderPseudoRandomGenerator	= regexp.MustCompile(`"PseudoRandomGenerator"`)
+    placeholderCoinToss			      	= regexp.MustCompile(`"CoinToss"`)
 )
 
 const (
-	filenameNonFungibleToken    	= "utility/NonFungibleToken.cdc"
-	filenameFungibleToken       	= "utility/FungibleToken.cdc"
-	filenameResolver            	= "utility/ViewResolver.cdc"
-	filenameMetadataViews       	= "utility/MetadataViews.cdc"
-	filenameRandomBeaconHistory 	= "RandomBeaconHistory.cdc"
-	filenamePseudoRandomGenerator	= "PseudoRandomGenerator.cdc"
-	filenameCoinToss				= "CoinToss.cdc"
+    filenameNonFungibleToken    	= "utility/NonFungibleToken.cdc"
+    filenameFungibleToken       	= "utility/FungibleToken.cdc"
+    filenameResolver            	= "utility/ViewResolver.cdc"
+    filenameMetadataViews       	= "utility/MetadataViews.cdc"
+    filenameRandomBeaconHistory 	= "RandomBeaconHistory.cdc"
+    filenamePseudoRandomGenerator	= "PseudoRandomGenerator.cdc"
+    filenameCoinToss				= "CoinToss.cdc"
 )
 
 // NonFungibleToken returns the NonFungibleToken contract interface.
 func NonFungibleToken() []byte {
-	code := assets.MustAssetString(filenameNonFungibleToken)
-	return []byte(code)
+    code := assets.MustAssetString(filenameNonFungibleToken)
+    return []byte(code)
 }
 
 // FungibleToken returns the FungibleToken contract interface.
 func FungibleToken() []byte {
-	return assets.MustAsset(filenameFungibleToken)
+    return assets.MustAsset(filenameFungibleToken)
 }
 
 func Resolver() []byte {
-	code := assets.MustAssetString(filenameResolver)
+    code := assets.MustAssetString(filenameResolver)
 
-	return []byte(code)
+    return []byte(code)
 }
 
 func MetadataViews(ftAddress, nftAddress, resolverAddress flow.Address) []byte {
-	code := assets.MustAssetString(filenameMetadataViews)
+    code := assets.MustAssetString(filenameMetadataViews)
 
-	code = placeholderFungibleToken.ReplaceAllString(code, "0x"+ftAddress.String())
-	code = placeholderNonFungibleToken.ReplaceAllString(code, "0x"+nftAddress.String())
-	code = placeholderResolver.ReplaceAllString(code, "0x"+resolverAddress.String())
+    code = placeholderFungibleToken.ReplaceAllString(code, "0x"+ftAddress.String())
+    code = placeholderNonFungibleToken.ReplaceAllString(code, "0x"+nftAddress.String())
+    code = placeholderResolver.ReplaceAllString(code, "0x"+resolverAddress.String())
 
-	return []byte(code)
+    return []byte(code)
 }
 
 func RandomBeaconHistory() []byte {
-	return assets.MustAsset(filenameRandomBeaconHistory)
+    return assets.MustAsset(filenameRandomBeaconHistory)
 }
 
 func PseudoRandomGenerator() []byte {
-	return assets.MustAsset(filenamePseudoRandomGenerator)
+    return assets.MustAsset(filenamePseudoRandomGenerator)
 }
--- lib/go/contracts/contracts_test.go
+++ lib/go/contracts/contracts_test.go
@@ -1,19 +1,19 @@
 package contracts_test
 
 import (
-	"testing"
+    "testing"
 
-	"github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/assert"
 
-	"github.com/onflow/random-coin-toss/lib/go/contracts"
+    "github.com/onflow/random-coin-toss/lib/go/contracts"
 )
 
 func TestRandomBeaconHistory(t *testing.T) {
-	contract := contracts.RandomBeaconHistory()
-	assert.NotNil(t, contract)
+    contract := contracts.RandomBeaconHistory()
+    assert.NotNil(t, contract)
 }
 
 func TestPseudoRandomGenerator(t *testing.T) {
-	contract := contracts.PseudoRandomGenerator()
-	assert.NotNil(t, contract)
+    contract := contracts.PseudoRandomGenerator()
+    assert.NotNil(t, contract)
 }
