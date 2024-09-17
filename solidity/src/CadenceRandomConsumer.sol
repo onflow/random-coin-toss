72e386b67aed39412677694b3862016d795185ec
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2024-09-16 18:33:00 -0600

    fix CoinToss.sol & add test coverage

--- solidity/src/CadenceRandomConsumer.sol
+++ solidity/src/CadenceRandomConsumer.sol
@@ -8,7 +8,7 @@ pragma solidity 0.8.19;
  */
 abstract contract CadenceRandomConsumer {
     // Cadence Arch pre-compile address
-    address constant public cadenceArch = 0x0000000000000000000000010000000000000001;
+    address public constant cadenceArch = 0x0000000000000000000000010000000000000001;
 
     // A struct to store the request details
     struct Request {
@@ -19,17 +19,8 @@ abstract contract CadenceRandomConsumer {
     }
 
     // Events
-    event RandomnessRequested(
-        uint256 requestId,
-        uint64 flowHeight,
-        uint256 evmHeight
-    );
-    event RandomnessFulfilled(
-        uint256 requestId,
-        uint64 flowHeight,
-        uint256 evmHeight,
-        uint256 randomResult
-    );
+    event RandomnessRequested(uint256 requestId, uint64 flowHeight, uint256 evmHeight);
+    event RandomnessFulfilled(uint256 requestId, uint64 flowHeight, uint256 evmHeight, uint256 randomResult);
 
     // A list of requests where each request is identified by its index in the array
     Request[] private _requests;
@@ -38,26 +29,27 @@ abstract contract CadenceRandomConsumer {
 
     /**
      * @dev This method serves as the commit step in this contract's commit-reveal scheme
-     * 
+     *
      * Here a caller places commits at Flow block n to reveal a random number at >= block n+1
      * This is because the random source for a Flow block is not available until after the finalization of that block.
      * Implementing contracts may wish to affiliate the request ID with the caller's address or some other identifier
      * so the relevant request can be fulfilled.
-     * Emits a {RandomnessRequested} event. 
-     * 
-     * @return requestId
+     * Emits a {RandomnessRequested} event.
+     *
+     * @return requestId The ID of the request.
      */
     function _requestRandomness() public returns (uint256) {
-        // Identify the request by the current request counter
+        // Identify the request by the current request counter, incrementing first so implementations can use 0 for
+        // invalid requests - e.g. myRequests[msg.sender] == 0 means the caller has no pending requests
+        _requestCounter++;
         uint256 requestId = _requestCounter;
         // Store the heights at which the request was made. Note that the Flow block height and EVM block height are
         // not the same. But since Flow blocks ultimately determine usage of secure randomness, we gate requests by
         // Flow block height.
         Request memory request = Request(_getFlowBlockHeight(), block.number);
 
-        // Store the request and increment the request counter
+        // Store the request in the list of requests
         _requests.push(request);
-        _requestCounter++;
 
         emit RandomnessRequested(requestId, request.flowHeight, request.evmHeight);
 
@@ -67,28 +59,30 @@ abstract contract CadenceRandomConsumer {
 
     /**
      * @dev This method serves as the reveal step in this contract's commit-reveal scheme
-     * 
+     *
      * Here a caller reveals a random number at least one block after the commit block
      * This is because the random source for a Flow block is not available until after the finalization of that block.
      * Note that the random source for a given Flow block is singular. In order to ensure that requests made at the same
      * block height are unique, implementing contracts should use some pseudo-random method to generate a unique value
      * from the seed along with a salt.
-     * Emits a {RandomnessFulfilled} event. 
-     * 
-     * @param requestId
-     * @return randomResult
+     * Emits a {RandomnessFulfilled} event.
+     *
+     * @param requestId The ID of the randomness request to fulfill.
+     * @return randomResult The random value generated from the Flow block.
      */
     function _fulfillRandomness(uint32 requestId) internal returns (uint64) {
-        // Get the request details
-        require(requestId < _requests.length, "Invalid request ID - value exceeds the number of existing requests");
-        Request memory request = _requests[requestId];
+        // Get the request details. Recall that request IDs are 1-indexed to allow for 0 to be used as an invalid value
+        uint32 requestIndex = requestId - 1;
+        require(requestIndex < _requests.length, "Invalid request ID - value exceeds the number of existing requests");
+        Request memory request = _requests[requestIndex];
 
         // Ensure that the request is fulfilled at a Flow block height greater than the one at which the request was made
         uint64 flowHeight = _getFlowBlockHeight();
         require(request.flowHeight < flowHeight, "Cannot fulfill request until subsequent Flow network block height");
 
         // Get the random source for the Flow block at which the request was made
-        uint64 randomResult = _getRandomSource(request.flowHeight);
+        uint64 randomResult = _getRandomSource(request.flowHeight); // returns bytes32
+        // Pack the randomResult into a uint64, hashing with the requestId to vary results across shared block heights
         randomResult = uint64(uint256(keccak256(abi.encodePacked(randomResult, requestId))));
 
         emit RandomnessFulfilled(requestId, request.flowHeight, request.evmHeight, randomResult);
@@ -99,8 +93,8 @@ abstract contract CadenceRandomConsumer {
 
     /**
      * @dev This method returns the current Flow block height
-     * 
-     * @return flowBlockHeight
+     *
+     * @return flowBlockHeight The current Flow block height.
      */
     function _getFlowBlockHeight() internal view returns (uint64) {
         (bool ok, bytes memory data) = cadenceArch.staticcall(abi.encodeWithSignature("flowBlockHeight()"));
@@ -113,9 +107,9 @@ abstract contract CadenceRandomConsumer {
     /**
      * @dev This method uses the Cadence Arch pre-compiles to returns a random source for a given Flow block height.
      * The provided height must be at least one block in the past.
-     * 
-     * @param flowHeight
-     * @return randomSource
+     *
+     * @param flowHeight The Flow block height for which to get the random source.
+     * @return randomSource The random source for the given Flow block height.
      */
     function _getRandomSource(uint64 flowHeight) private view returns (uint64) {
         (bool ok, bytes memory data) =
--- solidity/src/CoinToss.sol
+++ solidity/src/CoinToss.sol
@@ -3,28 +3,80 @@ pragma solidity 0.8.19;
 
 import "./CadenceRandomConsumer.sol";
 
+/**
+ * @dev This contract is a simple coin toss game where users can place win prizes by flipping a coin.
+ */
 contract CoinToss is CadenceRandomConsumer {
-    uint8 constant betMultiplier = 2;
+    // A constant to store the multiplier for the prize
+    uint8 public constant multiplier = 2;
 
+    // A mapping to store the request ID for each user
     mapping(address => uint256) public coinTosses;
-    mapping(uint256 => uint256) public bets;
+    // A mapping to store the value sent by the user for each request
+    mapping(uint256 => uint256) public openRequests;
 
-    function flipCoin() public payable {
-        require(msg.value > 0, "Must send ETH to place a bet");
-        uint256 requestId = _requestRandomness();
-        coinTosses[msg.sender] = requestId;
-        bets[requestId] = msg.value;
+    event CoinFlipped(address indexed user, uint256 requestId, uint256 amount);
+    event CoinRevealed(address indexed user, uint256 requestId, uint8 coinFace, uint256 prize);
+
+    /**
+     * @dev Checks if a user has an open request.
+     */
+    function hasOpenRequest(address user) public view returns (bool) {
+        return coinTosses[user] != 0;
     }
 
+    /**
+     * @dev Checks if a value is non-zero.
+     */
+    function isNonZero(uint256 value) public pure returns (bool) {
+        return value > 0;
+    }
+
+    /**
+     * @dev Allows a user to flip a coin by sending FLOW to the contract. This is the commit step in the commit-reveal scheme.
+     */
+    function flipCoin() public payable {
+        require(isNonZero(msg.value), "Must send FLOW to place flip a coin");
+        require(!isNonZero(coinTosses[msg.sender]), "Must close previous coin flip before placing a new one");
+
+        // request randomness
+        uint256 requestId = _requestRandomness();
+        // insert the request ID into the coinTosses mapping
+        coinTosses[msg.sender] = requestId;
+        // insert the value sent by the sender with the flipCoin function call into the openRequests mapping
+        openRequests[requestId] = msg.value;
+
+        emit CoinFlipped(msg.sender, requestId, msg.value);
+    }
+
+    /**
+     * @dev Allows a user to reveal the result of the coin flip and claim their prize.
+     */
     function revealCoin() public {
+        require(hasOpenRequest(msg.sender), "Caller has not flipped a coin - nothing to reveal");
+
         // reveal random result and calculate winnings
         uint256 requestId = coinTosses[msg.sender];
+        // delete the open request from the coinTosses mapping
+        delete coinTosses[msg.sender];
+
+        // fulfill the random request
         uint64 randomResult = _fulfillRandomness(uint32(requestId));
-        uint256 bet = bets[requestId];
-        uint256 winnings = bet * betMultiplier;
-        if (randomResult % 2 == 0) {
-            // win
-            payable(msg.sender).transfer(winnings);
+        uint8 coinFace = uint8(randomResult % 2);
+
+        // get the value sent in the flipCoin function & remove the request from the openRequests mapping
+        uint256 amount = openRequests[requestId];
+        delete openRequests[requestId];
+
+        // calculate the prize
+        uint256 prize = 0;
+        // send the prize if the random result is even
+        if (coinFace == 0) {
+            prize = amount * multiplier;
+            bool sent = payable(msg.sender).send(prize); // Use send to avoid revert
+            require(sent, "Failed to send prize");
         }
+
+        emit CoinRevealed(msg.sender, requestId, coinFace, prize);
     }
 }
